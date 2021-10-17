ff0eadb9563eb78923b01ac43eccc313b03ea263
AdahMilly <oamildred@gmail.com>
2021-10-17 15:11:40 +0300

    add pagination

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import React, {useState, useEffect} from "react"
 import axios from "axios"
+import Posts from "./components/Posts"
+
+import "./App.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Pagination from "./components/Pagination";
 
 const App = () => {
 
   const [posts,setPosts ] = useState([])
   const [loading, setLoading] = useState(false)
   const [currentPage, setCurrentPage] = useState(1)
-  const [postsPerPage, setPostsPerPage] = useState(100)
+  const [postsPerPage] = useState(10)
 
   useEffect(() => {
     const fetchPosts = async() => {
@@ -18,10 +23,21 @@ const App = () => {
     fetchPosts()
   }, [])
 
+  //get current post
+  const indexOfLastPost = currentPage * postsPerPage
+  const indexOfFirstPage = indexOfLastPost - postsPerPage
+  const currentPosts = posts.slice(indexOfFirstPage, indexOfLastPost)
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber)
+  }
+
 
   return (
-    <div>
-      <h1>React pagination</h1>
+    <div className="container mt-5">
+      <h1 className="text-primary mb-3">React pagination</h1>
+      <Posts posts={currentPosts} loading={loading}/>
+      <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} paginate={paginate}/>
     </div>
   );
 }
